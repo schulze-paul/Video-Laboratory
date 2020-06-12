1950b0d8298582052378ea9a53687ecc30e17af9
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2020-06-13 00:35:14 +0500

    adding dynamic buttons functionality

--- electron/YouTube_API_Test/view.js
+++ electron/YouTube_API_Test/view.js
@@ -9,7 +9,9 @@ var apiChannelURL = "https://www.googleapis.com/youtube/v3/channels";
 var channelPart = "snippet, statistics";
 
 // global sorted data variable:
-sortedVideoData = {};
+var sortedVideoData = {};
+// global variable with the answers to the questions
+var questionAnswer = [];
 
 function goButtonPressed() {
   // function is executed when the button is pressed,
@@ -183,82 +185,82 @@ function writeDataExcelFile(sortedData, fileName) {
   // A table header
 
   // find locations of all the data which can be put into the excel sheet
-  {
-    const fourLetter = sheet.getCell("C1");
-    const recordingDate = sheet.getCell("D1");
-    const codingPerson = sheet.getCell("E1");
-    const videoLink = sheet.getCell("F1");
-    const videoTitle = sheet.getCell("G1");
-    const ownId = sheet.getCell("H1");
-    const videoDuration = sheet.getCell("I1");
-    const channelName = sheet.getCell("J1");
-    const channelSubs = sheet.getCell("Q1");
-    const personalWebsite = sheet.getCell("S1");
-    const tiktokBin = sheet.getCell("T1");
-    const tiktokLink = sheet.getCell("U1");
-    const instagramBin = sheet.getCell("V1");
-    const instagramLink = sheet.getCell("W1");
-    const facebookBin = sheet.getCell("X1");
-    const facebookLink = sheet.getCell("Y1");
-    const twitterBin = sheet.getCell("Z1");
-    const twitterLink = sheet.getCell("AA1");
-    const otherSocialmedia = sheet.getCell("AB1");
-    const videoDate = sheet.getCell("AG1");
-    const channelJoin = sheet.getCell("AD1");
-    const channelVideoCount = sheet.getCell("AE1");
-    const videoViews = sheet.getCell("AH1");
-    const videoUpvotes = sheet.getCell("AI1");
-    const videoDownvotes = sheet.getCell("AJ1");
-    const commentsCount = sheet.getCell("AX1");
-    const commentsPinned = sheet.getCell("AY1");
-  }
 
-  // data from the questionnaire
-  {
-    questionCell = [];
-    question[10] = sheet.getCell("K1");
-    question[11] = sheet.getCell("L1");
-    question[12] = sheet.getCell("M1");
-    question[13] = sheet.getCell("N1");
-    question[14] = sheet.getCell("O1");
-    question[15] = sheet.getCell("P1");
-    question[17] = sheet.getCell("R1");
-    question[28] = sheet.getCell("AC1");
-    question[31] = sheet.getCell("AF1");
-    question[36] = sheet.getCell("AK1");
-    question[37] = sheet.getCell("AL1");
-    question[38] = sheet.getCell("AM1");
-    question[39] = sheet.getCell("AN1");
-    question[40] = sheet.getCell("AO1");
-    question[41] = sheet.getCell("AP1");
-    question[42] = sheet.getCell("AQ1");
-    question[43] = sheet.getCell("AR1");
-    question[44] = sheet.getCell("AS1");
-    question[45] = sheet.getCell("AT1");
-    question[46] = sheet.getCell("AU1");
-    question[47] = sheet.getCell("AV1");
-    question[48] = sheet.getCell("AW1");
-    question[51] = sheet.getCell("AZ1");
-    question[52] = sheet.getCell("BA1");
-    question[53] = sheet.getCell("BB1");
-    question[54] = sheet.getCell("BC1");
-    question[55] = sheet.getCell("BD1");
-    question[56] = sheet.getCell("BE1");
-    question[57] = sheet.getCell("BF1");
-    question[58] = sheet.getCell("BG1");
-    question[59] = sheet.getCell("BH1");
-    question[60] = sheet.getCell("BI1");
-    question[61] = sheet.getCell("BJ1");
-    question[62] = sheet.getCell("BK1");
-    question[63] = sheet.getCell("BL1");
-    question[64] = sheet.getCell("BM1");
-    question[65] = sheet.getCell("BN1");
-    question[66] = sheet.getCell("BO1");
-    question[67] = sheet.getCell("BP1");
-    question[68] = sheet.getCell("BQ1");
-  }
+  const fourLetter = sheet.getCell("C1");
+  const recordingDate = sheet.getCell("D1");
+  const codingPerson = sheet.getCell("E1");
+  const videoLink = sheet.getCell("F1");
+  const videoTitle = sheet.getCell("G1");
+  const ownId = sheet.getCell("H1");
+  const videoDuration = sheet.getCell("I1");
+  const channelName = sheet.getCell("J1");
+  const channelSubs = sheet.getCell("Q1");
+  const personalWebsite = sheet.getCell("S1");
+  const tiktokBin = sheet.getCell("T1");
+  const tiktokLink = sheet.getCell("U1");
+  const instagramBin = sheet.getCell("V1");
+  const instagramLink = sheet.getCell("W1");
+  const facebookBin = sheet.getCell("X1");
+  const facebookLink = sheet.getCell("Y1");
+  const twitterBin = sheet.getCell("Z1");
+  const twitterLink = sheet.getCell("AA1");
+  const otherSocialmedia = sheet.getCell("AB1");
+  const videoDate = sheet.getCell("AG1");
+  const channelJoin = sheet.getCell("AD1");
+  const channelVideoCount = sheet.getCell("AE1");
+  const videoViews = sheet.getCell("AH1");
+  const videoUpvotes = sheet.getCell("AI1");
+  const videoDownvotes = sheet.getCell("AJ1");
+  const commentsCount = sheet.getCell("AX1");
+  const commentsPinned = sheet.getCell("AY1");
 
-  //put the video data into the specific cells
+  // locations of the data from the questionnaire
+
+  questionCell = [];
+  questionCell[10] = sheet.getCell("K1");
+  questionCell[11] = sheet.getCell("L1");
+  questionCell[12] = sheet.getCell("M1");
+  questionCell[13] = sheet.getCell("N1");
+  questionCell[14] = sheet.getCell("O1");
+  questionCell[15] = sheet.getCell("P1");
+  questionCell[17] = sheet.getCell("R1");
+  questionCell[28] = sheet.getCell("AC1");
+  questionCell[31] = sheet.getCell("AF1");
+  questionCell[36] = sheet.getCell("AK1");
+  questionCell[37] = sheet.getCell("AL1");
+  questionCell[38] = sheet.getCell("AM1");
+  questionCell[39] = sheet.getCell("AN1");
+  questionCell[40] = sheet.getCell("AO1");
+  questionCell[41] = sheet.getCell("AP1");
+  questionCell[42] = sheet.getCell("AQ1");
+  questionCell[43] = sheet.getCell("AR1");
+  questionCell[44] = sheet.getCell("AS1");
+  questionCell[45] = sheet.getCell("AT1");
+  questionCell[46] = sheet.getCell("AU1");
+  questionCell[47] = sheet.getCell("AV1");
+  questionCell[48] = sheet.getCell("AW1");
+  questionCell[51] = sheet.getCell("AZ1");
+  questionCell[52] = sheet.getCell("BA1");
+  questionCell[53] = sheet.getCell("BB1");
+  questionCell[54] = sheet.getCell("BC1");
+  questionCell[55] = sheet.getCell("BD1");
+  questionCell[56] = sheet.getCell("BE1");
+  questionCell[57] = sheet.getCell("BF1");
+  questionCell[58] = sheet.getCell("BG1");
+  questionCell[59] = sheet.getCell("BH1");
+  questionCell[60] = sheet.getCell("BI1");
+  questionCell[61] = sheet.getCell("BJ1");
+  questionCell[62] = sheet.getCell("BK1");
+  questionCell[63] = sheet.getCell("BL1");
+  questionCell[64] = sheet.getCell("BM1");
+  questionCell[65] = sheet.getCell("BN1");
+  questionCell[66] = sheet.getCell("BO1");
+  questionCell[67] = sheet.getCell("BP1");
+  questionCell[68] = sheet.getCell("BQ1");
+
+  console.log(questionCell);
+
+  //put the video data from the youtube api into the specific cells
 
   fourLetter.value = sortedData.fourLetter;
   ownId.value = sortedData.ownId;
@@ -278,6 +280,15 @@ function writeDataExcelFile(sortedData, fileName) {
   channelSubs.value = sortedData.channel.subsCount;
   channelVideoCount.value = sortedData.channel.videoCount;
 
+  // put the data from the questionaire into the specific cells
+  for (var i = 0; i < 70; i++) {
+    try {
+      questionCell[i].value = questionAnswer[i];
+    } catch (err) {
+      //question does not exist, try next question
+    }
+  }
+
   // Save Excel on Hard Disk
   workbook.xlsx.writeFile(fileName + ".xlsx").then(function () {
     // Success Message
@@ -286,19 +297,14 @@ function writeDataExcelFile(sortedData, fileName) {
 }
 
 function createForm(sortedData) {
-  var formId = 10;
-  var question = "Is the video a collaborative project?";
-  var typeInput = false;
-  var buttonLabel = [
-    "No",
-    "Yes, between Youtubers",
-    "Yes, with an External Partner",
-    "Not apparent",
-  ];
-  var buttonData = [77, 1, 2, 66];
+  for (var i = 0; i < 70; i++) {
+    try {
+      formId = questionData[i].formId;
+      question = questionData[i].question;
+      typeInput = questionData[i].typeInput;
 
-  if (typeInput == true) {
-    $("body").append(`
+      if (typeInput == true) {
+        $("body").append(`
       <article class="item" data-key="${formId}">
         <h4>${question}</h4>
         <input
@@ -312,49 +318,32 @@ function createForm(sortedData) {
       />
       </article>
     `);
-  }
+      }
 
-  if (typeInput == false) {
-    if ((buttonCount = 4)) {
-      $("body").append(`
-        <article class="item" data-key="article${formId}">
-          <h4>${question}</h4>
-          
-          <button
-          onclick="javasript:formButtonPressed(${formId},${buttonData[0]})"
-          class="btn btn-primary"
-          id="button0${formId}"
-          >
-          ${buttonLabel[0]}
-          </button>
-
-          <button
-          onclick="javasript:formButtonPressed(${formId},${buttonData[1]})"
-          class="btn btn-primary"
-          id="button1${formId}"
-          >
-          ${buttonLabel[1]}
-          </button>
-
-          <button
-          onclick="javasript:formButtonPressed(${formId},${buttonData[2]})"
-          class="btn btn-primary"
-          id="button2${formId}"
-          >
-          ${buttonLabel[2]}
-          </button>
-
-          <button
-          onclick="javasript:formButtonPressed(${formId},${buttonData[3]})"
-          class="btn btn-primary"
-          id="button3${formId}"
-          >
-          ${buttonLabel[3]}
-          </button>
-        </article>
-      `);
+      if (typeInput == false) {
+        buttonLabel = questionData[i].buttonLabel;
+        buttonData = questionData[i].buttonData;
+        $("body").append(
+          buttonArticleFun(formId, question, buttonData, buttonLabel)
+        );
+      }
+    } catch (err) {
+      //question does not exist, try next question
     }
   }
+
+  var formId = 10;
+  var question = "Is the video a collaborative project?";
+  var typeInput = false;
+  var buttonLabel = [
+    "No",
+    "Yes, between Youtubers",
+    "Yes, with an External Partner",
+    "Not apparent",
+  ];
+  var buttonData = [77, 1, 2, 66];
 }
 
-function formButtonPressed(formId, buttonData) {}
+function formButtonPressed(formId, buttonData) {
+  questionAnswer[formId] = buttonData;
+}
