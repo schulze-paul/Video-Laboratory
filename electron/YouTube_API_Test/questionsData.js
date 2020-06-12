956d5bf012780f414336f969ac7878d27bf4ba7f
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2020-06-12 13:53:47 +0500

    adding database of questions and buttons

---
+++ electron/YouTube_API_Test/questionsData.js
@@ -0,0 +1,402 @@
+questionData = [];
+
+questionData[10] = {
+    formId = 10,
+    question = "Is the video a collaborative project?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, between Youtubers",
+        "Yes, with an external partner",
+        "Not apparent",
+    ],
+    buttonData = [77, 1, 2, 66],
+
+};
+questionData[12] = {
+    formId = 12,
+    question = "Is a person visible on the thumbnail (preview picture)?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, the presenter or the presenters",
+        "Yes, a person the video relates to",
+        "Yes, different persons which the video relates",
+        "Reference unclear",
+        "Not apparent",
+
+    ],
+    buttonData = [77, 1, 2, 3, 4, 66],
+
+};
+questionData[13] = {
+    formId = 13,
+    question = "Is the thumbnail designed serious or is it sensational (clickbait)?",
+    typeInput = false,
+    buttonLabel = [
+        "No design",
+        "Serious",
+        "Incentive-generating design",       
+        "Sensational (clickbait)",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 4, 66],
+};
+questionData[14] = {
+    formId = 14,
+    question = "How is the thumbnail designed?",
+    typeInput = false,
+    buttonLabel = [
+        "Screenshot from video",
+        "Screenshot with text",
+        "Different picture with text",          
+        "Collage",
+        "Not apparent",
+    ],
+    buttonData = [1, 2, 3, 4, 66],
+};
+questionData[28] = {
+    formId = 28,
+    question = "How often are videos uploaded by the channel?",
+    typeInput = false,
+    buttonLabel = [
+        "Not apparent",
+        "Sporadically",
+        "Monthly",
+        "Weekly",
+        "Daily",
+        "Now inactive",
+    ],
+    buttonData = [66, 1, 2, 3, 4, 5],
+};
+questionData[31] = {
+    formId = 31,
+    question = "Approximate number of persons who are directly/indirectly involved in the video production",
+    typeInput = false,
+    buttonLabel = [
+        "One person",
+        "Team production",
+        "Not apparent",
+    ],
+    buttonData = [1, 2, 66],
+
+};
+questionData[39] = {
+    formId = 39,
+    question = "Are there links/URLs in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes, to own channel/website/Social Media/ podcasts",
+        "Yes, to the content of others",
+        "Yes, to advertisement/ sponsoring/affiliates/ links",
+        "Yes, multiple",
+        "Yes, hashtags",
+        "No",
+        "Description is empty",
+        "Not apparent",
+
+    ],
+    buttonData = [1, 2, 3, 4, 5, 6, 7, 66],
+
+};
+questionData[40] = {
+    formId = 40,
+    question = "Is there a video content- or topic introduction in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[41] = {
+    formId = 41,
+    question = "Is there information about the channel in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[42] = {
+    formId = 42,
+    question = "Are there credits or information about the contributors in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[43] = {
+    formId = 43,
+    question = "Is there information about the video context, respectively about the creation of the video?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[44] = {
+    formId = 44,
+    question = "Are there sources in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[45] = {
+    formId = 45,
+    question = "Are there Calls-to-Action in the video description",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[46] = {
+    formId = 46,
+    question = "Is there a summary of the content in the video description?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[47] = {
+    formId = 47,
+    question = "Does the video description reference a playlist that the video is part of?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Description is empty",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 66],
+};
+questionData[51] = {
+    formId = 51,
+    question = "Are there professional questions to the video content in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "Yes",
+        "No",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 66],
+};
+questionData[52] = {
+    formId = 52,
+    question = "If yes, how often to they appear?",
+    typeInput = false,
+    buttonLabel = [
+        "Rarely",
+        "Every now and then",
+        "Often",
+        "Does Not apply",
+    ],
+    buttonData = [ 1, 2, 3, 77],
+};
+questionData[53] = {
+    formId = 53,
+    question = "Are there professional statements to the video content in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes",
+        "Not apparent",
+    ],
+    buttonData = [ 77, 1, 2],
+};
+
+questionData[54] = {
+    formId = 54,
+    question = "If yes, how often do they appear?",
+    typeInput = false,
+    buttonLabel = [
+        "Rarely",
+        "Every now and then",
+        "Often",
+        "Does Not apply",
+    ],
+    buttonData = [ 1, 2, 3, 77],
+};
+
+questionData[55] = {
+    formId = 55,
+    question = "Are there proposals for further desired topics, for further videos, or for learning in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, proposed topics",
+        "Yes, video proposals",
+        "Yes, study ideas",
+        "Yes, several",
+        "Not apparent",
+
+    ],
+    buttonData = [77, 1, 2, 3, 4, 66],
+};
+
+questionData[56] = {
+    formId = 56,
+    question = "Are the suggestions for other desired topics from the comments on the channel implemented?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes",
+        "There are no proposals",
+        "Not apparent",
+
+    ],
+    buttonData = [77, 1, 2, 66],
+};
+
+
+questionData[57] = {
+    formId = 57,
+    question = "Is there feedback on the video design, e.g. I like that, you could have represented this differently, in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, a short phrase (on the surface) - e.g. I like the background, nice hat",
+        "Yes, feedback related to concrete content - e.g. Your image 1.23 was too confusing",
+        "Not apparent",
+,
+
+    ],
+    buttonData = [77, 1, 2, 66],
+};
+
+questionData[58] = {
+    formId = 58,
+    question = "Do commenters thank the video producer in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes",
+        "Not apparent",
+    ],
+    buttonData = [77, 1, 66],
+};
+
+questionData[59] = {
+    formId = 59,
+    question = "Do the commenters question the video contents and the sources critically in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, the contents",
+        "Yes, the sources",
+        "Yes, both",
+        "Not apparent",
+    ],
+    buttonData = [77, 1, 2, 3, 66],
+};
+questionData[60] = {
+    formId = 60,
+    question = "What are the comments like?",
+    typeInput = false,
+    buttonLabel = [
+        "exclusively positive",
+        "rather positive",
+        "balanced",
+        "rather negative",
+        "only negative",
+        "Not apparent",
+    ],
+    buttonData = [ 1, 2, 3, 4, 5, 66],
+};
+
+questionData[63] = {
+    formId = 63,
+    question = "Is there negative feedback to the producers in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, relating to the content",
+        "Yes, relating to the sources",
+        "Yes, relating to the person",
+        "Yes, relating to everything",
+        "Not apparent",
+
+    ],
+    buttonData = [77, 1, 2, 3, 4, 66],
+};
+
+
+questionData[64] = {
+    formId = 64,
+    question = "Is there a dialogue among the viewers in the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes",
+        "Not apparent",
+    ],
+    buttonData = [77, 1, 66],
+};
+questionData[65] = {
+    formId = 65,
+    question = "Do the comments refer to the appearances of the people visible in the video?",
+    typeInput = false,
+    buttonLabel = [
+        "No visual people apparent",
+        "Yes",
+        "Yes, appreciative",
+        "Yes, derogatory",
+        "No",
+],
+    buttonData = [66, 1, 2, 3, 4],
+};
+
+questionData[67] = {
+    formId = 67,
+    question = "How intensive do the producers react through their own comments related to the comments?",
+    typeInput = false,
+    buttonLabel = [
+        "Not at all",
+        "Every once in a while",
+        "Often",
+        "Mostly",
+        "Always",  
+        "Mainly through likes",
+        "Not apparent",  
+],
+    buttonData = [77, 1, 2, 3, 4, 5, 66],
+};
+
+
+questionData[68] = {
+    formId = 68,
+    question = "Do the producers ask the viewers to comment on the content or the design of the video?",
+    typeInput = false,
+    buttonLabel = [
+        "No",
+        "Yes, to the content",
+        "Yes, to the design",
+        "Not apparent",  
+],
+    buttonData = [77, 1, 2, 66],
+};
