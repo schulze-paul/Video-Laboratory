c251a57be9c0d427b6dec3d776c8846582badafa
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2021-08-07 13:48:35 +0200

    changing logo

--- react/video/src/App.js
+++ react/video/src/App.js
@@ -3,21 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1></h1>
+      <label for="videoLinkInputLabel">Video Link</label>
+      <input
+        type="link"
+        name="videoLinkInput"
+        value="https://www.youtube.com/watch?v=WbEGmghn_jo"
+        id="videoLinkInput"
+        placeholder="Video Link"
+        class="form-control"
+        required
+      />
+      <label for="ownIdInputLabel">Set the ID</label>
+      <input
+        type="text"
+        name="ownIdInput"
+        value=""
+        id="ownIdInput"
+        placeholder="Own ID"
+        class="form-control"
+        required
+      />
+      <label
+        onClick="javascript:goButtonPressed();location.href='./new_approach/form_page.html';"
+        class="btn btn-primary"
+        id="submitVideoLink"
+      >
+        Go!
+      </label>
+
+      <script src="./apiKey.js"></script>
+      <script src="./new_approach/new_approach.js"></script>
+      <script src="./new_approach/video_dataset.js"></script>
+      <script src="./new_approach/html_handling.js"></script>
+
     </div>
   );
 }
