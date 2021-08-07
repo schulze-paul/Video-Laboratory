28252bea7d6bc4e5e992ee532116fcbcb8538ced
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2021-08-07 14:27:50 +0200

    trying split view

--- electron/new_approach/new_approach.js
+++ electron/new_approach/new_approach.js
@@ -1,13 +1,6 @@
 
-
-// raw_video_data = download_video_data(options, video_link)
-// raw_channel_data = download_channel_data(options, channel_id)
-// full_data = transform_data(raw_video_data, raw_channel_data)
-// write_data_to_excel(full_data, name of file?)
-
 // imports
 const $ = require("jquery");
-
 
 // global variables
 var dataset
@@ -36,5 +29,3 @@ async function goButtonPressed() {
     
 }
 
-
-  
