2c0b725d98492a12081809a67d8cc382b27970d2
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2021-08-08 12:17:57 +0200

    implementing split pane

--- video-laboratory/src/App.js
+++ video-laboratory/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import SplitPane from 'react-split-pane';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <SplitPane split="vertical" defaultSize="50%">
+          <div className="left-pane">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+              </a>
+            </header>
+          </div>
+          <div className="view-pane">
+          </div>
+        </SplitPane>
     </div>
+             
   );
 }
 
