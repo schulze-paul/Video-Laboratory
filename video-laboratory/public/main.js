513047d8a89ae5f850c32fcf74e23883a3c835f6
bl4ckp4nther4 <bl4ckp4nther4@gmail.com>
2021-08-08 10:17:50 +0200

    creating a stable dev process

---
+++ video-laboratory/public/main.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow.loadURL('http://localhost:3000');
+
+  app.setAboutPanelOptions({
+    applicationName: "Mook",
+    applicationVersion: "0.0.1",
+  })
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
